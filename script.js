68d33bd6a502bf5e26e09edd56728c168a84677f
kcccendana <kc_telyn@yahoo.com>
2023-07-03 22:45:56 +0900

    add eventlistener for botton and addBook function

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
 const addButton = document.querySelector('#add');
-let itemArray = [];
-
-let bookObject = {
-    title: '',
-    author: '',
-  };
+let itemArray = JSON.parse(localStorage.getItem('bookItems')) || [];
 
 addButton.addEventListener('click', () => {
-    bookObject.title = title.value;
-    bookObject.author = author.value;
-    itemArray.push(bookObject);
-    localStorage.setItem(bookData, JSON.stringify(itemArray));
-})
+    const titleInput = document.querySelector('#title');
+    const authorInput = document.querySelector('#author');
+    const title = titleInput.value;
+    const author = authorInput.value;
+    addBook(title, author);
+});
 
+function addBook(title, author) {
+    const newBook = { title: title, author: author };
+    itemArray.push(newBook);
+    localStorage.setItem('bookItems', JSON.stringify(itemArray));
+    displayBooks();
+}
 
-console.log(itemArray);
