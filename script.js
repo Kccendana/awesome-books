fe936b61f77e39cfd3ae9af9370be69bddd031d5
kcccendana <kc_telyn@yahoo.com>
2023-07-05 21:28:54 +0900

    add script to manipulate the title heading of booklist

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-use-before-define */
 /* eslint-disable max-classes-per-file */
-
 class Book {
   constructor(title, author) {
     this.title = title;
@@ -11,13 +10,14 @@ class Book {
 class BookList {
   constructor() {
     this.booksArray = JSON.parse(localStorage.getItem('bookItems')) || [];
+
   }
 
   addBook(title, author) {
     const book = new Book(title, author);
     this.booksArray.push(book);
     localStorage.setItem('bookItems', JSON.stringify(this.booksArray));
-    this.displayBooks();
+    // this.displayBooks();
   }
 
   removeBook(index) {
@@ -29,19 +29,25 @@ class BookList {
   displayBooks() {
     const bookListDiv = document.querySelector('#book-container');
     bookListDiv.innerHTML = '';
+
+    const booklistHeading = document.querySelector('.list-title');
+    if (this.booksArray.length === 0) {
+      bookListDiv.style.border = 'none';
+      booklistHeading.innerHTML = 'No book on the list';
+    } else {
+      booklistHeading.innerHTML = 'All awesome books';
+    }
+
     this.booksArray.forEach((book, index) => {
       bookListDiv.innerHTML += `
       <li><span><strong>"${book.title}"</strong> by <strong>${book.author}</strong></span><button class="button" id="remove" data-index="${index}">Remove</button></li>`;
       bookListDiv.style.border = '2px solid';
       const removeBtns = document.querySelectorAll('.button');
-
+     
       removeBtns.forEach((button) => {
         button.addEventListener('click', (event) => {
           const { index } = event.target.dataset;
           this.removeBook(index);
-          if (this.booksArray.length === 0) {
-            bookListDiv.style.border = 'none';
-          }
         });
       });
     });
